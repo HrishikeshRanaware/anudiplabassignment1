c06113f8fff2e8207a964e79b1b972f481274acc
Lenovo <Lenovo@DESKTOP-Q74AOCJ>
2022-11-11 12:29:06 +0530

    My First Commit

---
+++ Assignment 5/src/changecase/changecase.java
@@ -0,0 +1,24 @@
+package changecase;
+
+public class changecase {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s = "HeLlo";
+		String changedCase = "";
+		for(int i = 0; i< s.length(); i++ )
+		{
+			char c = s.charAt(i); 
+			if(Character.isUpperCase(c)) {
+				changedCase = changedCase + Character.toLowerCase(c);
+				
+			}
+			else {
+				changedCase = changedCase + Character.toUpperCase(c);
+			}
+		}
+		System.out.println(changedCase);
+
+	}
+
+}
